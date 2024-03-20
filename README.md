126644624d3774a4fdf4d7a4ead9fe287a66c624
Derek Burgess <contact@derekburgess.com>
2024-03-20 18:24:30 -0400

    Updates

--- README.md
+++ README.md
@@ -7,16 +7,22 @@ A simple Google Form, Google Sheet, and GitHub issue system for managing anythin
 
 ### Google Form
 
-First, create a google form and populate it with any fields you need. The script uses 2 fields to create an issue title, and then populates the issue body with all of the fields, making this mvp scalable.
+First, create a google form and populate it with any fields you need. The script uses 2 fields to create an issue title, and then populates the issue body with all of the fields, making this mvp scalable. It is a good idea to enable restrictions and/or input validation where needed.
 
 ### Google Sheets
 
 Simply enable the Google Sheet support for the Google Form and it will autocreate and associate the sheet.
 
+Later, if checking the sheet, take note that we call the sheet, not the spreadsheet:
+
+`var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Form Responses 1");`
+
 
 ### App Script
 
-In the associated Google Sheet, open Extensions > App Script and copy the included `app_script.js` into it.
+There are two version, a very basic "allow all" script, `app_script_nocheck.js` and a slightly more advanced version that checks against a limited number (5) of unique values for a given field, preventing additional github issues from being created, while still logging the form submissions in the sheet. That script is `app_script_check.js`.
+
+In the associated Google Sheet, open Extensions > App Script and copy/paste `app_script_` into it.
 
 Next navigate to the App Script Triggers UI, Clock icon, and add a new trigger.
 
@@ -36,6 +42,14 @@ The app script requires 3 things:
 
 With your GitHub parameters in the app script, try to submit a form. Alot can go wrong with configurations here, but generally try to keep everything named consistantly and it should work.
 
+When using `app_script_check.js`, the script will keep track of the submission number and append labels to the git issue, `submission: 1`, `submission: 2`, etc.
+
+There is no error for end-users submitting more than 5 submissions... Copy should be provided in the form intro and success message to warn them of this limitation.
+
 In Google App Script UI, you can configure triggers to notify you immediately and this will include errors.
 
-A demo form can be found here: https://forms.gle/xrE7EHwsy5HXzeoNA -- which feeds into this repositoties issues.
+Demo issues can be seen in the /issues section of this repo.
+
+Lastly, the point of keeping all form submissions in the spreadsheet is for auditing against the git issues and if needed, elevating missed submissions.
+
+An improvement could be to pass the git issue id back to the row in the spreadsheet for later validation.
--- app_script_check.js
+++ app_script_check.js
@@ -3,63 +3,62 @@ var USER_NAME = '';
 var REPO_NAME = '';
 
 function createGitHubIssue(e) {
-  var formResponse = e.namedValues;
+    var formResponse = e.namedValues;
+    var discordHandle = formResponse['Discord Handle'][0];
+    var email = formResponse['Email'][0];
+    var project = formResponse['Project Name'][0];
+    var issueTitle = "Submission from: " + discordHandle + ", Project: " + project;
   
-  var discordHandle = formResponse['Discord Handle'][0];
-  var project = formResponse['Project Name'][0];
-  var issueTitle = "Submission from: " + discordHandle + ", Project: " + project;
-
-  var issueBody = "";
-  var fieldOrder = [
-    'Name/Handle',
-    'Discord Handle',
-    'Email',
-    'Project Name',
-    'Project Description',
-    'Links to Project'
-  ];
-
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Form Responses 1");
-  var data = sheet.getDataRange().getValues();
+    var issueBody = "";
+    var fieldOrder = [
+      'Name/Handle',
+      'Discord Handle',
+      'Email',
+      'Project Name',
+      'Project Description',
+      'Links to Project'
+    ];
   
-  var discordHandleCount = 0;
-  
-  for (var i = 1; i < data.length; i++) {
-    if (data[i][1] === discordHandle) {
-        discordHandleCount++;
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Form Responses 1");
+    var data = sheet.getDataRange().getValues();
+    var emailCount = 0;
+    
+    for (var i = 1; i < data.length; i++) {
+      if (data[i][3] === email) {
+          emailCount++;
+      }
     }
-  }
-
-  var submissionCount = Math.max(discordHandleCount);
   
-  if (submissionCount > 5) {
-    return;
-  }
-
-  fieldOrder.forEach(function(field) {
-    if (formResponse.hasOwnProperty(field)) {
-      issueBody += field + ": " + formResponse[field][0] + "\n";
+    if (emailCount > 5) {
+      return;
     }
-  });
-
-  var url = 'https://api.github.com/repos/' + USER_NAME + '/' + REPO_NAME + '/issues';
-  var payload = {
-    "title": issueTitle,
-    "body": issueBody,
-    "labels": ["volunteer_submission", "submission: " + (submissionCount + 0)]
-  };
-
-  var options = {
-    "method": "post",
-    "headers": {
-      "Authorization": "token " + GITHUB_TOKEN,
-      "Accept": "application/vnd.github.v3+json"
-    },
-    "contentType": "application/json",
-    "payload": JSON.stringify(payload),
-    "muteHttpExceptions": false
-  };
-
-  var response = UrlFetchApp.fetch(url, options);
-  Logger.log(response.getContentText());
-}
+  
+    var submissionCount = Math.max(emailCount);
+    
+    fieldOrder.forEach(function(field) {
+      if (formResponse.hasOwnProperty(field)) {
+        issueBody += field + ": " + formResponse[field][0] + "\n";
+      }
+    });
+  
+    var url = 'https://api.github.com/repos/' + USER_NAME + '/' + REPO_NAME + '/issues';
+    var payload = {
+      "title": issueTitle,
+      "body": issueBody,
+      "labels": ["volunteer_submission", "submission: " + submissionCount]
+    };
+  
+    var options = {
+      "method": "post",
+      "headers": {
+        "Authorization": "token " + GITHUB_TOKEN,
+        "Accept": "application/vnd.github.v3+json"
+      },
+      "contentType": "application/json",
+      "payload": JSON.stringify(payload),
+      "muteHttpExceptions": false
+    };
+  
+    var response = UrlFetchApp.fetch(url, options);
+    Logger.log(response.getContentText());
+  }
